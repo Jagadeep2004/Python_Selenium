f9d4e1743799dcd8e84fa08741cd1719f7feb6a0
jagadeep <jagadeep2004@gmail.com>
2026-06-09 22:48:41 +0530

    new

--- TutorialsNinjaDemo/Tests/test_login.py
+++ TutorialsNinjaDemo/Tests/test_login.py
@@ -2,11 +2,15 @@ import pytest
 import configparser
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from Utilities.excelReader import get_data
 from Utilities.logCreater import log_generator
 
 config = configparser.RawConfigParser()
 config.read("./Utilities/config.ini")
+
 
 @pytest.mark.parametrize("email,password",get_data("./ExcelFiles/login_data.xlsx", "valid_login"))
 def test_login(setup, email, password):
@@ -15,21 +19,25 @@ def test_login(setup, email, password):
 
     driver = setup
 
+    wait = WebDriverWait(driver, 20)
+
     logger.info("Opening TutorialsNinja Website")
 
     driver.get(config.get("common info", "baseURL"))
 
-    driver.find_element(By.XPATH, "//span[text()='My Account']").click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'My Account')]"))).click()
 
-    driver.find_element(By.LINK_TEXT, "Login").click()
+    wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Login"))).click()
 
     logger.info(f"Entering Email : {email}")
 
-    driver.find_element(By.ID, "input-email").send_keys(email)
+    wait.until(EC.visibility_of_element_located((By.ID, "input-email"))).send_keys(email)
 
     driver.find_element(By.ID, "input-password").send_keys(password)
 
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()
+    driver.find_element(By.XPATH,"//input[@value='Login']").click()
+
+    wait.until(EC.url_contains("account"))
 
     assert "account" in driver.current_url.lower()
 
